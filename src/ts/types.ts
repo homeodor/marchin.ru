faa6f4e5b2a3f770227ee59dcc104ae9b9b4f04f
Alexander Golovanov <a.golovanov@atol.ru>
2024-01-23 11:51:34 +0300

    Formatting

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,38 +1,38 @@
 export interface Streaming {
-  [index: string]: string;
+	[index: string]: string;
 }
 
 export interface OneTrack {
-  title: string;
-  feat: string;
-  version: string;
-  cover: string;
+	title: string;
+	feat: string;
+	version: string;
+	cover: string;
 }
 
 export interface Tracklist {
-  header?: string;
-  tracks: (string | OneTrack)[];
+	header?: string;
+	tracks: (string | OneTrack)[];
 }
 
 export interface Stats {
-  tracks: number;
-  minutes: number;
-  omissions: number;
-  startyear: number;
+	tracks: number;
+	minutes: number;
+	omissions: number;
+	startyear: number;
 }
 
 export interface Data {
-  id: string;
-  title: string;
-  year: number;
-  kind: string;
-  description: string;
-  workingtitles: string[];
-  tracklist: Tracklist[];
-  stats: Stats;
+	id: string;
+	title: string;
+	year: number;
+	kind: string;
+	description: string;
+	workingtitles: string[];
+	tracklist: Tracklist[];
+	stats: Stats;
 }
 
 export interface Release {
-  data: Data;
-  streaming: Streaming;
+	data: Data;
+	streaming: Streaming;
 }
